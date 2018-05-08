b60a24575f3b845046f41d0c77b2aa4ed85dd1e2
Yves Roulin <y.roulin@gmail.com>
2018-05-08 17:13:17 -0600

    Added some comments to the express index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // requires the express
+const app = express(); // creates the express object
+
+/* Express Router Handler which takes 2 parameters, the route, and an arrow function
+   The arrow function has also two parameters: request and response, the res.send sends an JSON object with that dummy data
+   If you navigate to localhost:5000 it will appear { "hi" : "there" } on the screen
+   app.listen listens to any request from the port 5000.
+*/
 
 app.get('/', (req, res) => {
     res.send({
